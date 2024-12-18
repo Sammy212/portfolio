ffecec89f95ebef37759b3d343401ebc05c4eb05
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-18 13:35:49 +0100

    Tailwind Styles Config

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+    return (
+        <div className="fle">
+            <nav className="nav">
+                <a href="">Home</a>
+                <a href="">Projects</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </nav>
+        </div>
+    )
+};
