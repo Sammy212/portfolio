8ba4068b05addb97837c0acf8523c383d4e2a50f
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-19 13:25:33 +0100

    Added Myprojects Component

---
+++ src/data/data.ts
@@ -0,0 +1,44 @@
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+
+export const myProjects = [
+  {
+    title: "TheUnforsaken",
+    year: "2024",
+    description: "The Unforsaken-House of Hephzibah",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://theunforsaken.org",
+    image: darkSaasLandingPage,
+  },
+  {
+    title: "shopLux",
+    year: "2024",
+    description: "Light Saas Landing Page",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://shoplux-next.vercel.app/",
+    githubLink: "https://github.com/Sammy212/shoplux-next",
+    image: lightSaasLandingPage,
+  },
+  {
+    title: "Calendly",
+    year: "2024",
+    description: "A Calendly Replica",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://calendly-theta.vercel.app/",
+    githubLink: "https://github.com/Sammy212/calendly",
+    image: aiStartupLandingPage,
+  },
+];
