2c7fd988624388285d3042af837a069d61f5b547
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-20 11:59:03 +0100

    Added Testimonials compenent

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,9 @@
+import testimonialAvatar1 from "@/assets/images/testimonial-ava-1.jpg";
+import testimonialAvatar2 from "@/assets/images/testimonial-ava-2.jpg";
+import testimonialAvatar3 from "@/assets/images/testimonial-ava-3.jpg";
+import testimonialAvatar4 from "@/assets/images/testimonial-ava-4.jpg";
+import testimonialAvatar5 from "@/assets/images/testimonial-ava-5.jpg";
+
 import ShopLux from "@/assets/images/shoplux.png";
 import TheUnforsaken from "@/assets/images/theunforsaken.png";
 import Calendly from "@/assets/images/calendly.png";
@@ -10,6 +16,8 @@ import CalendlyIcon from "@/assets/icons/Calendly-icon.png";
 import Nodejs from "@/assets/icons/node-js-icon.png";
 import Stripe from "@/assets/icons/stripe.webp";
 
+
+// Projects Data
 export const myProjects = [
   {
     id: 3,
@@ -109,6 +117,8 @@ export const myProjects = [
   },
 ];
 
+
+// Strengths data
 export const myStrengths = [
   "Communicative",
   "Active Listener",
@@ -125,3 +135,37 @@ export const myStrengths = [
   "Time management",
 ]
 
+
+// testimonials data
+export const testimonials = [
+  {
+    name: "Alex Turner",
+    position: "Marketing Manager @ TechStartups",
+    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    avatar: testimonialAvatar1,
+  },
+  {
+    name: "Olivia Green",
+    position: "Head of Design @ GreenLeaf",
+    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    avatar: testimonialAvatar2,
+  },
+  {
+    name: "Daniel White",
+    position: "CEO @ InnovateCo",
+    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    avatar: testimonialAvatar3,
+  },
+  {
+    name: "Emily Carter",
+    position: "Product Manager @ GlobalTech",
+    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    avatar: testimonialAvatar4,
+  },
+  {
+    name: "Michael Brown",
+    position: "Director of IT @ MegaCorp",
+    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    avatar: testimonialAvatar5,
+  },
+];
