454ffc1e810365a5f340e5cd245fbf32ba6e8d05
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-20 22:48:43 +0100

    finished Testimonials compenent

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/Card";
 
 export function Testimonials() {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Client"
@@ -13,13 +13,13 @@ export function Testimonials() {
           description="Don't just take my word for it. See what my clients have to say about my work"
         />
 
-        <div className="mt-16 flex">
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex gap-8 flex-none">
             {
               testimonials.map(testimonial => (
                 <Card 
                   key={testimonial.name}
-                  className="max-w-xs"
+                  className="max-w-xs md:p-8 md:max-w-md"
                 >
                   <div className="flex gap-4 items-center">
                     <Image
@@ -28,11 +28,11 @@ export function Testimonials() {
                     />
                     <div>
                       <p className="font-semibold">{testimonial.name}</p>
-                      <p className="text-sm text-white/40">{testimonial.position}</p>
+                      <p className="text-sm text-white/40 md:text-base">{testimonial.position}</p>
                       <p className="bg-gradient-to-r from-[#DA498D] to-[#ed9d3c]/80 inline-flex gap-2 font-bold tracking-widest text-xs text-transparent bg-clip-text">{testimonial.company}</p>
                     </div>
                   </div>
-                  <p className="mt-4 text-sm text-white/80">{testimonial.text}</p>
+                  <p className="mt-4 md:mt-6 text-sm text-white/80">{testimonial.text}</p>
                 </Card>
               ))
             }
