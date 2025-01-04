60d514addea17d7c0f99d0ca43ee665d759d48af
Samuel Afolabi <samuelafo.212@gmail.com>
2025-01-04 09:04:12 +0100

    Integrated hotJar for better UI/UX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({
@@ -30,6 +31,22 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Script
+          // Hotjar Tracking Code for https://samuel-afo.vercel.app/ 
+          // to track and understand user's behavior to improve on User Experience
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5256511,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </body>
     </html>
   );
